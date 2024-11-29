5c3204627451784a20030374d99a91577d4e4209
carlosflores30 <robertoflores302630@gmail.com>
2024-11-29 15:50:35 -0500

    Initial Commit

---
+++ ChatAppiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppiOS
+//
+//  Created by Roberto Flores on 29/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
